5aa8e6378bde99ba1f8c50bafc70192b20ab6c17
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2025-03-17 22:28:58 +0100

    Implement detail view

---
+++ MushiViewer/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  MushiViewer
+//
+//  Created by Serrano Soria, Juan on 17/03/2025.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MushiViewer/ViewController.swift
+++ MushiViewer/ViewController.swift
@@ -35,6 +35,13 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let viewController = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            viewController.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
 
 }
 
